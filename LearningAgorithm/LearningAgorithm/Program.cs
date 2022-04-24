cc43b33bb86b8b93f4e0ca944829dde504ff8dd8
tuan.bui <tuan.bui@icomm.vn>
2022-04-24 15:25:18 +0700

    first commit

---
+++ LearningAgorithm/LearningAgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningAgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
